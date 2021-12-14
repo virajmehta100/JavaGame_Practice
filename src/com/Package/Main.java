03005bcea933cb08c12ae47b9f84b6159a166971
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-14 22:34:39 +0530

    near completion

--- src/com/Package/Main.java
+++ src/com/Package/Main.java
@@ -3,7 +3,9 @@ package com.Package;
 public class Main {
 
     public static void main(String[] args) {
-
-
+        Player1 player = new Player1("Viraj","sword",100);
+        System.out.println(player.getName());
+        System.out.println(player.getName());
+        System.out.println(player.getName());
     }
 }
--- src/com/Package/Player2.java
+++ src/com/Package/Player2.java
@@ -23,5 +23,26 @@ public class Player2 extends Player1 {
             System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 30." +
                     "New health is " + this.health);
         }
+        if(this.health == 0){
+            System.out.println(getName() + " is dead..");
+        }
+    }
+
+    @Override
+    public void damageByGun2() {
+        if(armour){
+            this.health -= 40;
+            if(this.health <= 0) this.health = 0;
+            System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 40." +
+                    "New health is " + this.health);
+        }if(!armour){
+            this.health -= 50;
+            if(this.health <= 0) this.health = 0;
+            System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 50." +
+                    "New health is " + this.health);
+        }
+        if(this.health == 0){
+            System.out.println(getName() + " is dead..");
+        }
     }
 }
