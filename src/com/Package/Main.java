ac13aba72548d877b7f134aaf9107eceda2bb976
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-14 22:46:53 +0530

    reaching the end

--- src/com/Package/Main.java
+++ src/com/Package/Main.java
@@ -8,6 +8,11 @@ public class Main {
 //        System.out.println(player.getHealth());
 //        System.out.println(player.getWeapon());
 
-        player.damageByGun1();
+//        player.damageByGun1();
+//        player.damageByGun1();
+//        player.damageByGun2();
+
+        Player2 betterPlayer = new Player2("Manav","machine gun",80,false);
+        betterPlayer.damageByGun1();
     }
 }
--- src/com/Package/Player2.java
+++ src/com/Package/Player2.java
@@ -20,7 +20,7 @@ public class Player2 extends Player1 {
         }if(!armour){
             this.health -= 30;
             if(this.health <= 0) this.health = 0;
-            System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 30." +
+            System.out.println("Armour is off. Got hit by gun 1. Health is reduced by 30." +
                     "New health is " + this.health);
         }
         if(this.health == 0){
@@ -38,7 +38,7 @@ public class Player2 extends Player1 {
         }if(!armour){
             this.health -= 50;
             if(this.health <= 0) this.health = 0;
-            System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 50." +
+            System.out.println("Armour is off. Got hit by gun 1. Health is reduced by 50." +
                     "New health is " + this.health);
         }
         if(this.health == 0){
