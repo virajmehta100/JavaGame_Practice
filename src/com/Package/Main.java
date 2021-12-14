61921d64ac5b2f7a86db5c698af59af4f053a47b
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-14 21:58:37 +0530

    first commit

---
+++ src/com/Package/Main.java
@@ -0,0 +1,9 @@
+package com.Package;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/com/Package/Player1.java
@@ -0,0 +1,13 @@
+package com.Package;
+
+public class Player1 {
+    private String name;
+    private String weapon;
+    private int health;
+
+    public Player1(String name, String weapon, int health) {
+        this.name = name;
+        this.weapon = weapon;
+        this.health = health;
+    }
+}
