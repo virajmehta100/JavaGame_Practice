8b75b624965608d6c191abff59330a81796ab147
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-14 22:41:05 +0530

    new health feature

--- src/com/Package/Main.java
+++ src/com/Package/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Player1 player = new Player1("Viraj","sword",100);
-        System.out.println(player.getName());
-        System.out.println(player.getName());
-        System.out.println(player.getName());
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+
+        player.damageByGun1();
     }
 }
