fea0ce863f63baeafd1dd2b194dc92402383d390
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-14 22:28:07 +0530

    new features

--- src/com/Package/Player2.java
+++ src/com/Package/Player2.java
@@ -1,15 +1,27 @@
 package com.Package;
 
-public class Player2 {
-    private String name;
-    private String weapon;
+public class Player2 extends Player1 {
     private int health;
+    private boolean armour;
 
-    public Player2(String name, String weapon, int health) {
-        this.name = name;
-        this.weapon = weapon;
+    public Player2(String name, String weapon, int health, boolean armour) {
+        super(name, weapon, health);
         this.health = health;
+        this.armour = armour;
     }
 
-    
+    @Override
+    public void damageByGun1() {
+        if(armour){
+            this.health -= 20;
+            if(this.health <= 0) this.health = 0;
+            System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 20." +
+                    "New health is " + this.health);
+        }if(!armour){
+            this.health -= 30;
+            if(this.health <= 0) this.health = 0;
+            System.out.println("Armour is on. Got hit by gun 1. Health is reduced by 30." +
+                    "New health is " + this.health);
+        }
+    }
 }
